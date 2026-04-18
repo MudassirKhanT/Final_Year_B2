8f9a826b423bb3ca555c98c3f2d9f541bdbe776b
mudassir <mudassirkhantailor@gmail.com>
2026-04-18 10:34:23 +0530

    Final Commit

---
+++ my-final-year-app/src/app/api/clerk-webhook/route.ts
@@ -0,0 +1,37 @@
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    console.log("Wedbhook Hit");
+    const body = await req.json();
+    if (!body) {
+      return new NextResponse("Error body not present", { status: 500 });
+    }
+    const { id, email_addresses, first_name, image_url } = body?.data;
+
+    const email = email_addresses[0]?.email_address;
+    console.log("✅", body);
+
+    await db.user.upsert({
+      where: { clerkId: id },
+      update: {
+        email,
+        name: first_name,
+        profileImage: image_url,
+      },
+      create: {
+        clerkId: id,
+        email,
+        name: first_name || "",
+        profileImage: image_url || "",
+      },
+    });
+    return new NextResponse("User updated in database successfully", {
+      status: 200,
+    });
+  } catch (error) {
+    console.error("Error updating database:", error);
+    return new NextResponse("Error updating user in database", { status: 500 });
+  }
+}
